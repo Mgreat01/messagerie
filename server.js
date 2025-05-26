1a9ffe50f164ce47165dda257a64e32ab835b8f7
VotreNom <votre-email@example.com>
2025-05-26 16:55:25 +0200

    amelioreation du code

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
 
-app.use(express.static('public'));
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-  console.log('Un utilisateur est connecté');
+  console.log('Un utilisateur connecté');
 
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    console.log('Message reçu : ' + msg);
+    io.emit('chat message', msg); // renvoyer à tous les clients
   });
 
   socket.on('disconnect', () => {
@@ -17,7 +22,6 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3000;
-http.listen(PORT, () => {
-  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+server.listen(3000, () => {
+  console.log('Serveur en écoute sur http://localhost:3000');
 });
