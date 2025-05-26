2817a94abe49f738153691a6d7960f91f5ad71d8
VotreNom <votre-email@example.com>
2025-05-26 16:44:17 +0200

    ajout d un design pour l envois du message

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('Un utilisateur est connecté');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Un utilisateur s\'est déconnecté');
+  });
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
